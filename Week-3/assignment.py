18bc16efa06066ec9bf4f2041d1bd8ad06888376
bellaire <125338173+apeaceloner04@users.noreply.github.com>
2023-02-21 09:01:45 +0300

    assignment.py

---
+++ Week-3/assignment.py
@@ -0,0 +1,31 @@
+#create a list of empty of favourite musicians
+#using a for loop  add new musicians
+#copy the list called celebs
+#sort the list
+#pop out two celebs
+#count the remaining celebs in the list
+
+
+favourite_musicians=["Jessie murph","sam smith","bruno mars","miley cyrus","dean lewis"]
+for musician in favourite_musicians:
+    print(musician)
+favourite_musicians.append("rihanna")
+favourite_musicians.append("drake")
+favourite_musicians.append("meghan trainor")
+favourite_musicians.append("cardi b")
+for musician in favourite_musicians:
+    print(musician)
+
+celebs = favourite_musicians.copy()
+print(celebs)
+
+celebs.sort()
+print(celebs)
+
+celebs.pop()
+print(celebs)
+
+celebs.pop()
+print(celebs)
+
+print(len(celebs))
